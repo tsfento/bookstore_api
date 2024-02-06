b988a725c0bb577c8f96ccbe4d0c1d170317b02e
Tyler Fenton <tsfento@gmail.com>
2024-02-06 14:24:35 -0600

    init commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+    before_action :set_book, only: [:show, :update, :destroy]
+
+    def index
+        books = Book.all
+
+        render json: books, status: :ok
+    end
+
+    def show
+        render json: @book, status: :ok
+    end
+
+    def create
+        book = Book.new(book_params)
+
+        if book.save
+            render json: book, status: :created
+        else
+            render json: book.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @book.update(book_params)
+            render json: @book, status: :ok
+        else
+            render json: @book.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        if @book.destroy
+            render json: nil, status: :no_content
+        else
+            render json: @book.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def set_book
+        @book = Author.find(params[:id])
+    end
+
+    def book_params
+        params.require(:book).permit(:title)
+    end
+end
